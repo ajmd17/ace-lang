3c7c74585c6ad287a1b1a5bc453654dd1b84adfc
ajmd17 <andrewmaconald97@hotmail.com>
2016-09-26 18:30:05 -0700

    update build.py (again)

--- build.py
+++ build.py
@@ -1,14 +1,14 @@
 import os
 
-src = "./src"
-bin = "./bin"
+src_dir = "./src"
+bin_dir = "./bin"
 
-if not os.path.exists(bin):
-    os.makedirs(bin)
+if not os.path.exists(bin_dir):
+    os.makedirs(bin_dir)
 
-command = "g++ -o {}/athens -std=c++11 -Iinclude/".format(bin)
+command = "g++ -o {}/athens -std=c++11 -Iinclude/".format(bin_dir)
 
-for dirpath, dirnames, filenames in os.walk(src):
+for dirpath, dirnames, filenames in os.walk(src_dir):
     for file in [f for f in filenames]:
         if file.endswith(".cpp"):
             print("{}/{}...".format(dirpath, file))
